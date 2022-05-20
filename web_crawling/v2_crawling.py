91e9763f9cd62d0d988e358ad3efef595aeed869
LKM <kangmin7434@naver.com>
2022-05-20 17:52:51 +0000

    ADD: add version 1,2

--- web_crawling/v2_crawling.py
+++ web_crawling/v2_crawling.py
@@ -122,94 +122,110 @@ def yogiyo_crawling(location):
             time.sleep(2)          
             print('카페 검색 완료!')
 
+
             # 해당 카페 페이지 접속
-            search_selector = '#content > div > div:nth-child(5) > div > div > div:nth-child(1) > div'
-            search = driver.find_element_by_css_selector(search_selector)
-            search.click()
-            time.sleep(2)
-            print("카페 페이지 접속 완료!")
+            try:
+                search_selector = '#content > div > div:nth-child(5) > div > div > div:nth-child(1) > div'
+                search = driver.find_element_by_css_selector(search_selector)
+                search.click()
+                time.sleep(2)
+                print("카페 페이지 접속 완료!")
 
-            # 리뷰버튼 클릭
-            review_xpath = '''//*[@id="content"]/div[2]/div[1]/ul/li[2]/a'''
-            driver.find_element(by=By.XPATH, value=review_xpath).click()
-            time.sleep(2)
-
-            customer_xpath = '''//*[@id="content"]/div[2]/div[1]/div[5]/div[2]/div/strong[1]'''
-            custom_src = driver.find_element_by_xpath(customer_xpath)
-            owner_xpath = '''//*[@id="content"]/div[2]/div[1]/div[5]/div[2]/div/strong[2]'''
-            owner_src = driver.find_element_by_xpath(owner_xpath)
-            custom_len, owner_len = int(custom_src.text), int(owner_src.text)
-            print("리뷰수:", custom_len, " 답글수:", owner_len)
-
-            if custom_len//10 > owner_len:
-                print("답글이 10%도 안되서 다음 가게로....")
-                go_back_page()
-                time.sleep(3)
+            except Exception as e:
+                print("카페 페이지 접속 실패.. 다음 카페로...")
+                print(e)
                 continue
 
-            # 더보기버튼 클릭
-            start_time = time.time()
-            while True:
-                try:
-                    css_selector = '#review > li.list-group-item.btn-more > a'
-                    more_reviews = driver.find_element(by=By.CSS_SELECTOR, value=css_selector)
-                    more_reviews.click()
-                    time.sleep(2)
-                except:
-                    break
+            # 리뷰버튼 클릭
+            try:
+                review_xpath = '''//*[@id="content"]/div[2]/div[1]/ul/li[2]/a'''
+                driver.find_element(by=By.XPATH, value=review_xpath).click()
+                time.sleep(2)
 
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")  # 스크롤을 가장 아래로 내린다
-            time.sleep(2)
-            pre_height = driver.execute_script("return document.body.scrollHeight")  # 현재 스크롤 위치 저장
+                customer_xpath = '''//*[@id="content"]/div[2]/div[1]/div[5]/div[2]/div/strong[1]'''
+                custom_src = driver.find_element_by_xpath(customer_xpath)
+                owner_xpath = '''//*[@id="content"]/div[2]/div[1]/div[5]/div[2]/div/strong[2]'''
+                owner_src = driver.find_element_by_xpath(owner_xpath)
+                custom_len, owner_len = int(custom_src.text), int(owner_src.text)
+                print("리뷰수:", custom_len, " 답글수:", owner_len)
 
-            end_time = time.time()
-            print('리뷰 페이지 로드 완료!  ', end_time-start_time)
-            time.sleep(2)
-
-
-            html = driver.page_source
-            html_source = BeautifulSoup(html, 'html.parser')
-
-            tastes = []
-            quantitys = []
-            deliverys = []
-            menus = []
-            customer_reviews = []
-            manager_responses = []
-
-            full_review = html_source.find_all("li", attrs={"class":"list-group-item star-point ng-scope", "ng-repeat":"review in restaurant.reviews"})  # 전체 리뷰 스크립트
-
-            rname = html_source.find("span", class_="restaurant-name ng-binding")  # 업체명
-            print(rname.text, '-----카페 이름 확인')
-            print("데이터 추출 시작!")
-            for reviews in tqdm(full_review):
-                # 데이터 추출
-                manager_response = reviews.find_all("p", attrs={"class": "ng-binding", "ng-bind-html": "review.owner_reply.comment|strip_html"})
-
-                if len(manager_response) == 0: # 사장님 답글이 없을 경우 폐기
+                if custom_len//10 > owner_len:
+                    print("답글이 10%도 안되서 다음 가게로....")
+                    go_back_page()
+                    time.sleep(3)
                     continue
 
-                taste = reviews.find_all("span", attrs={"class": "points ng-binding", "ng-show": "review.rating_taste > 0"})
-                quantity = reviews.find_all("span", attrs={"class": "points ng-binding", "ng-show": "review.rating_quantity > 0"})
-                delivery = reviews.find_all("span", attrs={"class": "points ng-binding", "ng-show": "review.rating_delivery > 0"})
-                menu = reviews.find_all("div", class_="order-items default ng-binding")
-                customer_review = reviews.find_all("p", attrs={"class": "ng-binding", "ng-show": "review.comment"})
+                total_response += owner_len
+
+                # 더보기버튼 클릭
+                start_time = time.time()
+                while True:
+                    try:
+                        css_selector = '#review > li.list-group-item.btn-more > a'
+                        more_reviews = driver.find_element(by=By.CSS_SELECTOR, value=css_selector)
+                        more_reviews.click()
+                        time.sleep(2)
+                    except:
+                        break
+
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")  # 스크롤을 가장 아래로 내린다
+                time.sleep(2)
+                pre_height = driver.execute_script("return document.body.scrollHeight")  # 현재 스크롤 위치 저장
+
+                end_time = time.time()
+                print('리뷰 페이지 로드 완료!  ', end_time-start_time)
+                time.sleep(2)
 
 
-                tastes.append(taste[0].string if taste else '별점X')  # 별점-맛
-                quantitys.append(quantity[0].string if quantity else '별점X')  # 별점-양
-                deliverys.append(delivery[0].string if delivery else '별점X')  # 별점-배달
-                menus.append(menu[0].string if menu else '메뉴X')  # 주문 메뉴
-                customer_reviews.append(customer_review[0].string)  # 고객 리뷰
-                manager_responses.append(manager_response[0].string)  # 사장님 답글
+                html = driver.page_source
+                html_source = BeautifulSoup(html, 'html.parser')
 
-            reviews = pd.DataFrame({'업체명':restaurant_name, '맛':tastes,'양':quantitys,
-                            '배달':deliverys,'주문메뉴':menus, '고객리뷰':customer_reviews, '사장답글':manager_responses})
+                tastes = []
+                quantitys = []
+                deliverys = []
+                menus = []
+                customer_reviews = []
+                manager_responses = []
 
-            print("사장님 답글:",len(manager_responses))
-            total = pd.concat([total, reviews])
-            go_back_page()
-            time.sleep(2)
+                full_review = html_source.find_all("li", attrs={"class":"list-group-item star-point ng-scope", "ng-repeat":"review in restaurant.reviews"})  # 전체 리뷰 스크립트
+
+                rname = html_source.find("span", class_="restaurant-name ng-binding")  # 업체명
+                print(rname.text, '-----카페 이름 확인')
+                print("데이터 추출 시작!")
+                for reviews in tqdm(full_review):
+                    # 데이터 추출
+                    manager_response = reviews.find_all("p", attrs={"class": "ng-binding", "ng-bind-html": "review.owner_reply.comment|strip_html"})
+
+                    if len(manager_response) == 0: # 사장님 답글이 없을 경우 폐기
+                        continue
+
+                    taste = reviews.find_all("span", attrs={"class": "points ng-binding", "ng-show": "review.rating_taste > 0"})
+                    quantity = reviews.find_all("span", attrs={"class": "points ng-binding", "ng-show": "review.rating_quantity > 0"})
+                    delivery = reviews.find_all("span", attrs={"class": "points ng-binding", "ng-show": "review.rating_delivery > 0"})
+                    menu = reviews.find_all("div", class_="order-items default ng-binding")
+                    customer_review = reviews.find_all("p", attrs={"class": "ng-binding", "ng-show": "review.comment"})
+
+
+                    tastes.append(taste[0].string if taste else '별점X')  # 별점-맛
+                    quantitys.append(quantity[0].string if quantity else '별점X')  # 별점-양
+                    deliverys.append(delivery[0].string if delivery else '별점X')  # 별점-배달
+                    menus.append(menu[0].string if menu else '메뉴X')  # 주문 메뉴
+                    customer_reviews.append(customer_review[0].string)  # 고객 리뷰
+                    manager_responses.append(manager_response[0].string)  # 사장님 답글
+
+                reviews = pd.DataFrame({'업체명':restaurant_name, '맛':tastes,'양':quantitys,
+                                '배달':deliverys,'주문메뉴':menus, '고객리뷰':customer_reviews, '사장답글':manager_responses})
+
+                print("사장님 답글:",len(manager_responses))
+                total = pd.concat([total, reviews])
+                go_back_page()
+                time.sleep(2)
+
+            except Exception as e:
+                print("리뷰 크롤링 실패.. 다음 카페로..")
+                print(e)
+                go_back_page()
+                time.sleep(2)
 
     except Exception as e:
         print('@@카페 리스트 페이지 에러@@')
