9bc09549de95256be9636aff0f59f9116c6575fd
HyunAh-Kim-Clou <wgrape0@gmail.com>
2022-05-25 15:41:26 +0900

    FIX: masking_username apply && refactoring

--- data_preprocess/preprocessing.py
+++ data_preprocess/preprocessing.py
@@ -16,21 +16,19 @@ def concat_cafelist_files(DATA_DIR = "../data/", prefix_fname="total", output_fn
 
 def preprocessing_cafelist(input_fname="nonpre_total_cafes.csv", output_fname="pre_total_cafes.csv"):
     df = pd.read_csv(input_fname)
-    df = df.drop(['Unnamed: 0'], axis=1) 
     
     print(len(df['사장답글'].unique()))
     
     del_newline = lambda x: x.str.replace(r'[\\n\n]', ' ', regex=True) # \n 제거
     del_space = lambda x: x.str.replace(r'[\s\\n]{2,}', ' ', regex=True) # \n 여러 개 -> 1개 ' '
 
-    masking_username = lambda x: x.str.replace(r'[ㄱ-ㅎ가-힣\w]{0,}\*\*님', '[고객이름]님', regex=True) # 영어**님 -> [고객 이름]님
-    masking_username = lambda x: x.str.replace(r'[ㄱ-ㅎ가-힣\w]{0,}\*\*고객님', '[고객이름]고객님', regex=True)
-    masking_cafename = lambda x, cafe: re.sub(f"{cafe.split('-')[0]}", '[상호명]', x) # 카페이름 -> [상호명]
+    masking_username = lambda x: x.str.replace(r'[ㄱ-ㅎ가-힣\w]{0,}(\*\*){0,}(님|고객님)', '[고객이름]님', regex=True) # 영어**님 -> [고객 이름]님
+    masking_cafename = lambda x, cafe: re.sub(f"{cafe}|{' '.join(cafe.split('-'))}|{'|'.join(cafe.split('-'))}", '[상호명]', x) # 카페이름 -> [상호명]
     
     df['주문메뉴'] = del_space(del_newline(df['주문메뉴']))
     df['고객리뷰'] = del_space(del_newline(df['고객리뷰']))
     df['사장답글'] = masking_username(del_space(del_newline(df['사장답글'])))
-    df['사장답글'] = pd.Series(list(map(masking_cafename, df['사장답글'], df['업체명'])))
+    df['사장답글'] = list(map(masking_cafename, df['사장답글'], df['업체명']))
     
     df.to_csv(output_fname, encoding='utf-8')
     print('Pre-processing DONE!')
