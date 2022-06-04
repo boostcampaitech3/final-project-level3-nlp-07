d86d887f0695b53342cc29539301fbc8b539d8ab
HyunAh-Kim-Clou <wgrape0@gmail.com>
2022-06-04 12:50:57 +0900

    ADD: fastapi & streamlit installment

--- install/requirement.sh
+++ install/requirement.sh
@@ -3,3 +3,6 @@ pip install pandas==1.1.4
 pip install beautifulsoup4==4.11.1
 pip install tqdm==4.41.1
 pip install pororo==0.4.2
+pip install fastapi==0.68.1
+pip install uvicorn==0.15.0
+pip install streamlit==1.2.0
