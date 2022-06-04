eca64ed93d6e6ae298e9a8477f5c1b0c997ac704
HyunAh-Kim-Clou <wgrape0@gmail.com>
2022-06-04 14:04:11 +0900

    ADD: KoBART installment

--- install/requirement.sh
+++ install/requirement.sh
@@ -1,3 +1,4 @@
+# sh install/requirement.sh
 pip install selenium==3.141.0
 pip install pandas==1.1.4
 pip install beautifulsoup4==4.11.1
@@ -6,3 +7,7 @@ pip install pororo==0.4.2
 pip install fastapi==0.68.1
 pip install uvicorn==0.15.0
 pip install streamlit==1.2.0
+pip install datasets==2.2.2
+pip install wandb==0.12.17
+pip install torch==1.7.1
+pip install transformers==4.3.3
