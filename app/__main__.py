f4a329425fc610b14973ad2713be6a2ee15e2603
HyunAh-Kim-Clou <wgrape0@gmail.com>
2022-06-14 16:09:07 +0900

    FIX: delete app folder

--- app/__main__.py
+++
@@ -1,7 +0,0 @@
-# python3 -m app
-# python3 -m streamlit run app/frontend.py --server.port 30002 --server.fileWatcherType none
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run("app.backend:app", host="127.0.0.1", port=8001, reload=True)
